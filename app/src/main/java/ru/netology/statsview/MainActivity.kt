6c3b5aa0dc0b207ffbc42764c441bea094271012
dmitry-mak <akkaru@gmail.com>
2026-07-20 19:53:48 +0300

    Animation: base functionality

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -1,20 +1,50 @@
 package ru.netology.statsview
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import ru.netology.statsview.ui.StatsView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        findViewById<StatsView>(R.id.statsView).data = listOf(
+        val view = findViewById<StatsView>(R.id.statsView)
+        view.data = listOf(
             500F,
             500F,
             500F,
             500F
         )
+
+        val textView = findViewById<TextView>(R.id.label)
+
+        val animation =
+            AnimationUtils.loadAnimation(this, R.anim.animation).apply {
+                setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationEnd(p0: Animation?) {
+                        textView.text = "onAnimationEnd"
+                    }
+
+                    override fun onAnimationRepeat(p0: Animation?) {
+                        textView.text = "onAnimationRepeat"
+                    }
+
+                    override fun onAnimationStart(p0: Animation?) {
+                        textView.text = "onAnimationStart"
+                    }
+                })
+            }
+
+
+//        findViewById<StatsView>(R.id.statsView).startAnimation(
+//            AnimationUtils.loadAnimation(this, R.anim.animation)
+//        )
+
+        findViewById<StatsView>(R.id.statsView).startAnimation(animation)
     }
 }
 
